bf04612ac865feb95d602fa9296c407153ee2d53
anand-developer01 <67739417+anand-developer01@users.noreply.github.com>
2026-04-26 19:22:58 +0530

    Create users.js

---
+++ RTK/RTK Query(createApi)/Login API/node backend/DB/users.js
@@ -0,0 +1,47 @@
+[
+  {
+    "id": 1,
+    "username": "admin",
+    "password": "$2b$08$e4aChsRQmCRqQBd1kEsTqeao6wFmBnRT46s9JZR4b9RBFkuDV9Nr2",
+    "balance": 1918,
+    "transactions": [
+      {
+        "type": "DEPOSIT",
+        "amount": 454,
+        "date": "2026-04-25T16:44:14.356Z"
+      },
+      {
+        "type": "DEPOSIT",
+        "amount": 1,
+        "date": "2026-04-25T16:22:31.751Z"
+      },
+      {
+        "type": "DEPOSIT",
+        "amount": 2,
+        "date": "2026-04-25T16:20:38.184Z"
+      },
+      {
+        "type": "DEPOSIT",
+        "amount": 111,
+        "date": "2026-04-25T16:19:37.831Z"
+      },
+      {
+        "type": "DEPOSIT",
+        "amount": 250,
+        "date": "2026-04-25T15:45:06.633Z"
+      },
+      {
+        "type": "DEPOSIT",
+        "amount": 100,
+        "date": "2026-04-25T15:25:16.837Z"
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "username": "user1",
+    "password": "$2b$08$e4aChsRQmCRqQBd1kEsTqeao6wFmBnRT46s9JZR4b9RBFkuDV9Nr2",
+    "balance": 500,
+    "transactions": []
+  }
+]
